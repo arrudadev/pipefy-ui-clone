220647c44c5a98a4cb175a99547e69048d30b570
Alexandre <alexandre.monteiro.bec@gmail.com>
2020-09-22 22:49:17 -0300

    chore: add PipeHeader NavTop in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { FiBell, FiMoreVertical } from 'react-icons/fi';
 
-import { Container, Brand, Logo } from './styles';
+import {
+  Container,
+  Brand,
+  Logo,
+  PipeHeader,
+  NavTop,
+  NavBottom,
+  PipeTitle,
+  TrialUpate,
+  NavTopActions,
+  Notification,
+  Avatar,
+  Dots,
+} from './styles';
 
 const Header: React.FC = () => {
   return (
@@ -8,6 +22,31 @@ const Header: React.FC = () => {
       <Brand>
         <Logo />
       </Brand>
+      <PipeHeader>
+        <NavTop>
+          <PipeTitle>
+            <span>Chamados de TI</span>
+            <TrialUpate>
+              - <a>Atualize agora</a>
+            </TrialUpate>
+          </PipeTitle>
+          <NavTopActions>
+            <Notification>
+              <FiBell size={22} />
+            </Notification>
+            <Avatar>
+              <span>A</span>
+              <Dots>
+                <FiMoreVertical size={22} />
+              </Dots>
+            </Avatar>
+          </NavTopActions>
+        </NavTop>
+        <NavBottom>
+          <span>Visualizar em Kanban</span>
+          <span>Relatorios</span>
+        </NavBottom>
+      </PipeHeader>
     </Container>
   );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -11,7 +11,6 @@ export const Container = styled.header`
   color: #fff;
 
   display: flex;
-  align-items: center;
 `;
 
 export const Brand = styled.div`
@@ -42,3 +41,126 @@ export const Logo = styled(LogoWhite)`
   padding: 0;
   margin: 0;
 `;
+
+export const PipeHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+
+  padding-left: 12px;
+`;
+
+export const NavTop = styled.div`
+  height: 40px;
+
+  padding-top: 8px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const PipeTitle = styled.div`
+  font-size: 16px;
+  font-weight: bold;
+
+  cursor: pointer;
+
+  color: #fff;
+
+  display: flex;
+`;
+
+export const TrialUpate = styled.div`
+  color: #ffca78;
+
+  font-weight: bold;
+  font-size: 14px;
+
+  margin-left: 10px;
+
+  > a {
+    color: #ffca78;
+    font-weight: bold;
+    margin-left: 3px;
+    text-decoration: underline;
+
+    &:hover {
+      color: #fff;
+    }
+  }
+`;
+
+export const NavTopActions = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  max-width: 270px;
+`;
+
+export const Notification = styled.div`
+  > svg path {
+    color: rgba(255, 255, 255, 0.6);
+  }
+
+  &:hover {
+    cursor: pointer;
+
+    > svg path {
+      color: #fff;
+    }
+  }
+`;
+
+export const Avatar = styled.div`
+  display: flex;
+  align-items: center;
+
+  margin-left: 10px;
+
+  padding: 3px;
+
+  position: relative;
+
+  width: 60px;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.2);
+    border-radius: 3px;
+  }
+
+  > span {
+    max-height: 28px;
+    height: 28px;
+    width: 28px;
+
+    border-radius: 3px;
+
+    background: #179c20;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const Dots = styled.div`
+  padding-top: 4px;
+  padding-left: 6px;
+
+  > svg {
+    color: rgba(255, 255, 255, 0.6);
+  }
+
+  &:hover {
+    cursor: pointer;
+
+    > svg {
+      color: #fff;
+    }
+  }
+`;
+
+export const NavBottom = styled.div``;
